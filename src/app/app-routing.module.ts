8257cd839397edf2d1039225f01e55e2cafa2e07
Rafael Sánchez Montes <rafasanchezmontes1@gmail.com>
2022-02-22 14:07:10 +0100

    Componentes y Lazyload de formularios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveModule } from './reactive/reactive.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'template',
+    loadChildren: () => import('./template/template.module').then( m => m.TemplateModule )
+  },
+  {
+    path: 'reactive',
+    loadChildren: () => import('./reactive/reactive.module').then( m => m.ReactiveModule )
+  },
+  {
+    path: '**',
+    redirectTo: 'template'
+  }
+];
 
 @NgModule({
   imports: [
